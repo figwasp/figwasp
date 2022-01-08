d4b8fe2395d6082b4218cc102fde1c9008cc61c8
Joel Ling <joel-ling@protonmail.com>
2022-01-08 16:58:04 +0900

    add i/face Cluster + impl. for testing

---
+++ test/cluster.go
@@ -0,0 +1,62 @@
+package test
+
+import (
+	"sigs.k8s.io/kind/pkg/cluster"
+	"sigs.k8s.io/kind/pkg/cmd"
+	"sigs.k8s.io/kind/pkg/log"
+)
+
+type Cluster interface {
+	Destroy() error
+}
+
+type kindCluster struct {
+	provider       *cluster.Provider
+	name           string
+	kubeConfigPath string
+}
+
+func NewKindCluster(name, kubeConfigPath string) (c *kindCluster, e error) {
+	// Initialise a local Kubernetes cluster using Docker containers as nodes.
+
+	var (
+		logger log.Logger
+		option cluster.ProviderOption
+	)
+
+	logger = cmd.NewLogger()
+
+	option = cluster.ProviderWithLogger(logger)
+
+	c = &kindCluster{
+		provider:       cluster.NewProvider(option),
+		name:           name,
+		kubeConfigPath: kubeConfigPath,
+	}
+
+	e = c.Destroy()
+	if e != nil {
+		return
+	}
+
+	e = c.provider.Create(name)
+	if e != nil {
+		return
+	}
+
+	return
+}
+
+func (c *kindCluster) Destroy() (e error) {
+	//
+
+	e = c.provider.Delete(
+		c.name,
+		c.kubeConfigPath,
+	)
+	if e != nil {
+		return
+	}
+
+	return
+}
---
+++ test/cluster_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestCluster(t *testing.T) {
+	const (
+		clusterName    = "test-cluster"
+		kubeConfigPath = ""
+	)
+
+	var (
+		cluster Cluster
+		e       error
+	)
+
+	cluster, e = NewKindCluster(clusterName, kubeConfigPath)
+	if e != nil {
+		t.Error(e)
+	}
+
+	e = cluster.Destroy()
+	if e != nil {
+		t.Error(e)
+	}
+}
