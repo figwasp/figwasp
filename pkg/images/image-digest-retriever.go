477897c2b6094da439913c5071234a9cfa284720
Joel Ling <joel-ling@protonmail.com>
2022-03-20 14:57:42 +0900

    re-def. img. digest retriever i/face + impl

---
+++ pkg/images/image-digest-retriever.go
@@ -0,0 +1,163 @@
+package images
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/containers/image/v5/docker"
+	"github.com/containers/image/v5/manifest"
+	"github.com/containers/image/v5/types"
+	"github.com/opencontainers/go-digest"
+)
+
+type ImageDigestRetriever interface {
+	RetrieveImageDigest(string, context.Context) (string, error)
+}
+
+type imageDigestRetriever struct {
+	systemContext *types.SystemContext
+	pathsToRemove []string
+}
+
+func NewImageDigestRetriever(options ...imageDigestRetrieverOption) (
+	r *imageDigestRetriever, e error,
+) {
+	var (
+		option imageDigestRetrieverOption
+	)
+
+	r = &imageDigestRetriever{
+		systemContext: &types.SystemContext{},
+		pathsToRemove: []string{},
+	}
+
+	for _, option = range options {
+		e = option(r)
+		if e != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (r *imageDigestRetriever) RetrieveImageDigest(
+	imageReferenceString string, ctx context.Context,
+) (
+	imageDigestString string, e error,
+) {
+	const (
+		imageReferenceFormat = "//%s"
+	)
+
+	var (
+		imageCloser    types.ImageCloser
+		imageDigest    digest.Digest
+		imageManifest  []byte
+		imageReference types.ImageReference
+	)
+
+	imageReference, e = docker.ParseReference(
+		fmt.Sprintf(imageReferenceFormat, imageReferenceString),
+	)
+	if e != nil {
+		return
+	}
+
+	imageCloser, e = imageReference.NewImage(ctx, r.systemContext)
+	if e != nil {
+		return
+	}
+
+	imageManifest, _, e = imageCloser.Manifest(ctx)
+	if e != nil {
+		return
+	}
+
+	imageDigest, e = manifest.Digest(imageManifest)
+	if e != nil {
+		return
+	}
+
+	imageDigestString = imageDigest.String()
+
+	return
+}
+
+func (r *imageDigestRetriever) Destroy() (e error) {
+	var (
+		path string
+	)
+
+	for _, path = range r.pathsToRemove {
+		e = os.RemoveAll(path)
+		if e != nil {
+			return
+		}
+	}
+
+	return
+}
+
+type imageDigestRetrieverOption func(*imageDigestRetriever) error
+
+func WithBasicAuthentication(username, password string) (
+	option imageDigestRetrieverOption,
+) {
+	option = func(r *imageDigestRetriever) (e error) {
+		r.systemContext.DockerAuthConfig = &types.DockerAuthConfig{
+			Username: username,
+			Password: password,
+		}
+
+		return
+	}
+
+	return
+}
+
+func WithSelfSignedTLSCertificate(pathToCACert string) (
+	option imageDigestRetrieverOption,
+) {
+	option = func(r *imageDigestRetriever) (e error) {
+		const (
+			pathToCACertDirParent  = ""
+			pathToCACertDirPattern = "*"
+
+			pathToCACertLinkFormat = "%s/ca.crt"
+			// https://pkg.go.dev/
+			//  github.com/containers/image/v5/types#SystemContext
+			// > a directory containing a CA certificate (ending with ".crt")
+		)
+
+		var (
+			pathToCACertDir  string
+			pathToCACertLink string
+		)
+
+		pathToCACertDir, e = ioutil.TempDir(
+			pathToCACertDirParent,
+			pathToCACertDirPattern,
+		)
+		if e != nil {
+			return
+		}
+
+		pathToCACertLink = fmt.Sprintf(pathToCACertLinkFormat, pathToCACertDir)
+
+		e = os.Link(pathToCACert, pathToCACertLink)
+		if e != nil {
+			return
+		}
+
+		r.systemContext.DockerCertPath = pathToCACertDir
+
+		r.pathsToRemove = append(r.pathsToRemove, pathToCACertDir)
+
+		return
+	}
+
+	return
+}
---
+++ pkg/images/image-digest-retriever_test.go
@@ -0,0 +1,195 @@
+package images
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/opencontainers/go-digest"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/joel-ling/alduin/test/pkg/credentials"
+	"github.com/joel-ling/alduin/test/pkg/images"
+	"github.com/joel-ling/alduin/test/pkg/repositories"
+)
+
+func TestImageDigestRetrieverAgainstPublicRepository(t *testing.T) {
+	const (
+		imageRef0 = "golang:1.5.1"
+		// last updated 2015; Schema 1 manifest outdated
+		imageRef1 = "golang:1.10.1"
+		// last updated 2018; Schema 2 manifest
+
+		// obtained by pulling and inspecting images
+		imageDigestEncodedExpected0 = "" +
+			"23ca2c13e498feab91c5fa38f56d3b2bfaebc98d41d283102b467a2900e48e40"
+		imageDigestEncodedExpected1 = "" +
+			"4826b5c314a498142c7291ad835ab6be1bf02f7813d6932d01f1f0f1383cdda1"
+	)
+
+	var (
+		testCases map[string]string
+
+		retriever *imageDigestRetriever
+
+		imageDigestEncodedExpected string
+		imageRef                   string
+
+		imageDigest       digest.Digest
+		imageDigestString string
+
+		e error
+	)
+
+	testCases = map[string]string{
+		imageRef0: imageDigestEncodedExpected0,
+		imageRef1: imageDigestEncodedExpected1,
+	}
+
+	retriever, e = NewImageDigestRetriever()
+	if e != nil {
+		t.Error(e)
+	}
+
+	for imageRef, imageDigestEncodedExpected = range testCases {
+		imageDigestString, e = retriever.RetrieveImageDigest(
+			imageRef,
+			context.Background(),
+		)
+		if e != nil {
+			t.Error(e)
+		}
+
+		imageDigest = digest.Digest(imageDigestString)
+
+		assert.Equal(t,
+			digest.SHA256,
+			imageDigest.Algorithm(),
+		)
+
+		assert.Equal(t,
+			imageDigestEncodedExpected,
+			imageDigest.Encoded(),
+		)
+	}
+}
+
+func TestImageDigestRetrieverAgainstPrivateRepositoryWithBasicAuthAndTLS(
+	t *testing.T,
+) {
+	const (
+		repositoryHost = "127.0.0.1"
+		repositoryPort = 5000
+
+		buildContextPath = "../.."
+		dockerfilePath   = "test/build/dummy/Dockerfile"
+
+		imageName      = "dummy"
+		imageRefFormat = "%s:%d/%s"
+
+		username = "username"
+		password = "password"
+
+		imageDigestEncodedLength = 64
+	)
+
+	var (
+		credential *credentials.TLSCertificate
+
+		repository        *repositories.DockerRegistry
+		repositoryAddress net.TCPAddr
+
+		image    *images.DockerImage
+		imageRef string
+
+		retriever *imageDigestRetriever
+
+		imageDigest       digest.Digest
+		imageDigestString string
+
+		e error
+	)
+
+	credential, e = credentials.NewTLSCertificate(
+		credentials.WithExtendedKeyUsageForServerAuth(),
+		credentials.WithIPAddress(repositoryHost),
+	)
+	if e != nil {
+		t.Error(e)
+	}
+
+	defer credential.Destroy()
+
+	repositoryAddress = net.TCPAddr{
+		IP:   net.ParseIP(repositoryHost),
+		Port: repositoryPort,
+	}
+
+	repository, e = repositories.NewDockerRegistry(repositoryAddress,
+		repositories.WithBasicAuthentication(username, password),
+		repositories.WithTransportLayerSecurity(
+			credential.PathToCertPEM(),
+			credential.PathToKeyPEM(),
+		),
+	)
+	if e != nil {
+		t.Error(e)
+	}
+
+	defer repository.Destroy()
+
+	imageRef = fmt.Sprintf(imageRefFormat,
+		repositoryHost,
+		repositoryPort,
+		imageName,
+	)
+
+	image, e = images.NewDockerImage(buildContextPath, dockerfilePath,
+		images.WithTag(imageRef),
+		images.WithOutputStream(os.Stderr),
+	)
+	if e != nil {
+		t.Error(e)
+	}
+
+	defer image.Destroy()
+
+	e = image.PushWithBasicAuth(os.Stderr, username, password)
+	if e != nil {
+		t.Error(e)
+	}
+
+	retriever, e = NewImageDigestRetriever(
+		WithBasicAuthentication(username, password),
+		WithSelfSignedTLSCertificate(
+			credential.PathToCertPEM(),
+		),
+	)
+	if e != nil {
+		t.Error(e)
+	}
+
+	defer retriever.Destroy()
+
+	imageDigestString, e = retriever.RetrieveImageDigest(
+		imageRef,
+		context.Background(),
+	)
+	if e != nil {
+		t.Error(e)
+	}
+
+	imageDigest = digest.Digest(imageDigestString)
+
+	assert.Equal(t,
+		digest.SHA256,
+		imageDigest.Algorithm(),
+	)
+
+	assert.Equal(t,
+		imageDigestEncodedLength,
+		len(imageDigest.Encoded()),
+	)
+}
